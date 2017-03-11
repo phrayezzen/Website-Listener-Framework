aae71c7ae560ad2f5005612801afc23d1e123b9c
Xilin Liu <xal1@rice.edu>
2017-03-10 23:20:59 -0800

    added switch listener

--- jobs.py
+++ jobs.py
@@ -2,40 +2,43 @@ import json
 from urllib2 import build_opener
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
-
-
 from SMTPConnection import *
 
+
+
 opener = build_opener()
-opener.addheaders = [("User-agent", "Mozilla/5.0")]
-with open("/Users/phrayezzen/Documents/Projects/Website-Listener-Framework/webs.json") as data:
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+with open('/Users/phrayezzen/MASTER/Projects/Website-Listener-Framework/webs.json') as data:
     webs = json.load(data)
 smtp = SMTPConnection()
-smtp.login(webs["username"], webs["password"], webs["smtpServer"])
-timestamp = datetime.now()
+smtp.login(webs['username'], webs['password'], webs['smtpServer'])
 
-for site in webs["sites"]:
-    for job_id in webs["sites"][site]:
-        job = webs["sites"][site][job_id]
-        target = bs(opener.open(job["URL"]).read())
+for site in webs['sites']:
+    for job in webs['sites'][site]:
+        desc = job['description']
+        print 'Polling for ' + desc
+        target = bs(opener.open(job['URL']).read())
 
         is_present = True
-        for tag in job["path"]:
+        for tag in job['path']:
             try:
-                target = target.find(tag["name"], id=tag["id"], class_=tag["class"]) if "class" in tag else \
-                    target.find(tag["name"], id=tag["id"])
-            except:
+                target = target.find(tag['name'], id=tag['id'], class_=tag['class']) if 'class' in tag else \
+                    target.find(tag['name'], id=tag['id'])
+            except Exception as e:
+                print e
                 is_present = False
                 break
         if not is_present:
             continue
 
-        if job["target"] in target.get_text():
-            message = "You have a new website update! The listener sends you a message:\n\n" + \
-                "Description: " + job["description"] + \
-                "\n" + job["target"] + " was found on the website.\n" + job["message"] + \
-                "\n\nHope that helps! Thanks for using this website listener! Contact alexliu7@yahoo.com for problems." + \
-                "\nSent at " + str(datetime.now())
-            smtp.send_message(webs["username"], job["recipients"], message)
+        if job['target'] in target.get_text():
+            message = 'You have a new website update! The listener sends you a message:\n\n' + \
+                'Description: ' + desc + \
+                '\n' + job['target'] + ' was found on the website.\n' + job['message'] + \
+                '\n\n' + job['URL'] + \
+                '\n\nHope that helps! Thanks for using this website listener! Contact xaliu7@gmail.com for problems.' + \
+                '\nSent at ' + str(datetime.now())
+            print 'Sending message now for ' + desc
+            smtp.send_message(webs['username'], job['recipients'], message)
 
 smtp.logout()
--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
-"""
+'''
 setup
 create json file
 ask for user/pass/serv
 create crontab
-"""
+'''
 import json
 import os
 
@@ -11,19 +11,19 @@ import os
 class ListenerUpdater(object):
     def __init__(self):
         self.webs = {}
-        with open(os.path.join("/Users/phrayezzen/Documents/Projects/Website-Listener-Framework", 'webs.json')) as data:
+        with open(os.path.join('/Users/phrayezzen/Documents/Projects/Website-Listener-Framework', 'webs.json')) as data:
             self.webs = json.load(data)
-            self.num_sites = self.webs["numSites"]
+            self.num_sites = self.webs['numSites']
 
     def setup(self):
-        self.webs["username"] = raw_input("Enter Username:\n")
-        self.webs["password"] = raw_input("Enter Password:\n")
-        self.webs["smtpServer"] = raw_input("Enter mail server URL:\n")
+        self.webs['username'] = raw_input('Enter Username:\n')
+        self.webs['password'] = raw_input('Enter Password:\n')
+        self.webs['smtpServer'] = raw_input('Enter mail server URL:\n')
 
     def add(self):
-        URL = raw_input("Paste the URL on which you want to listen to:\n")
+        URL = raw_input('Paste the URL on which you want to listen to:\n')
         root = self.get_root(URL)  # TODO get root
-        if root in self.webs["sites"]:
+        if root in self.webs['sites']:
             pass
 
     def save(self):
@@ -32,14 +32,14 @@ class ListenerUpdater(object):
     def get_root(url):
         pass
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     listener = ListenerUpdater()
     while True:
-        choice = raw_input("What would you like to do? Enter a number.\n" +
-                           "1 - Setup/change login info\n" +
-                           "2 - Add new listeners\n" +
-                           "3 - View/delete listeners\n" +
-                           "4 - Exit")
+        choice = raw_input('What would you like to do? Enter a number.\n' +
+                           '1 - Setup/change login info\n' +
+                           '2 - Add new listeners\n' +
+                           '3 - View/delete listeners\n' +
+                           '4 - Exit')
         if choice == 1:
             listener.setup()
         elif choice == 2:
